4d97a7303070ad12d136734d032dbcd0ffb6b64a
typhain <typhaine.charrat@gmail.com>
2019-08-22 23:47:13 +0200

    acné

---
+++ script_7.js
@@ -0,0 +1,13 @@
+user_input = prompt("Dis quelque chose")
+
+if (user_input.includes("?")){
+  console.log("Ouais Ouais...")
+  } else if (user_input == 0){
+    console.log("t'es en PLS ?")
+    } else if (user_input == user_input.toUpperCase()){
+  console.log("Pwa, calme-toi...")
+      } else if (user_input.includes("Fortnite")){
+  console.log("on s' fait une partie soum-soum ?")
+        } else {
+    console.log("balek.")
+          }
